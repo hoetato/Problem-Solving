8e50bcf4f02f69198d9e843999cd0616ee624b5c
caroline <carolinebless94@gmail.com>
2021-10-10 00:44:42 +0700

    problem 7

---
+++ Project Problem Solving/Assets/Scripts/Problem 7/BoxSpawner7.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner7 : MonoBehaviour
+{
+    [SerializeField] GameObject boxPrefab;
+    [SerializeField] int maxNumberOfBox;
+    [SerializeField] float areaWidth, areaHeight, objectSizeMax;
+    float areaOffset;
+    int numberOfBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        numberOfBox = Random.Range(5, maxNumberOfBox + 1);
+        areaOffset = 0.5f;
+        float x, y;
+        GameObject box;
+
+        for (int i = 0; i < numberOfBox; i++)
+        {
+
+            x = Random.RandomRange(-areaWidth + areaOffset, areaWidth - areaOffset);
+            y = Random.RandomRange(-areaHeight + areaOffset, areaHeight - areaOffset);
+            box = Instantiate(boxPrefab, new Vector2(x, y), Quaternion.identity);
+            box.transform.localScale = new Vector2(Random.Range(0, objectSizeMax) + 0.5f, Random.Range(0, objectSizeMax) + 0.5f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
