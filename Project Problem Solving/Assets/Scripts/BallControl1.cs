e97bbfb91dbb92c5c31960469b1ddec92b22ad1b
caroline <carolinebless94@gmail.com>
2021-10-09 22:06:32 +0700

    problem 2

---
+++ Project Problem Solving/Assets/Scripts/BallControl1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl1 : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        rigidbody.velocity = new Vector2(2, 2);
+    }
+
+
+}
