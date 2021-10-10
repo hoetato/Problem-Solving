3e9608c29b50f4727e805eb22db3b9410671192e
caroline <carolinebless94@gmail.com>
2021-10-10 14:45:42 +0700

    commit terakhir bye

---
+++ Project Problem Solving/Assets/Scripts/Problem 10/SceneManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public void Problem1() 
+    {
+       UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void Problem2()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+    }
+
+    public void Problem3()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(3);
+    }
+
+    public void Problem4()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(4);
+    }
+
+    public void Problem5()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(5);
+    }
+
+    public void Problem6()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(6);
+    }
+
+    public void Problem7()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(7);
+    }
+
+    public void Problem8()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(8);
+    }
+
+    public void Problem9()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(9);
+    }
+
+    public void BackToMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+
+}
