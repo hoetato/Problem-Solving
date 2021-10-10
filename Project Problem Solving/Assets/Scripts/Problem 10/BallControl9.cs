806d0bc8aabb130215598ce514619cb4e01573b6
caroline <carolinebless94@gmail.com>
2021-10-10 13:50:17 +0700

    commit main

---
+++ Project Problem Solving/Assets/Scripts/Problem 10/BallControl9.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl9 : MonoBehaviour
+{
+    public float maxMoveSpeed = 10;
+    public float smoothTime = 0.3f;
+    Rigidbody2D rigidbody;
+
+    Vector2 currentVelocity;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rigidbody.position = Vector2.SmoothDamp(rigidbody.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
+        transform.Rotate(0f, 0f, Time.deltaTime * -100f);
+    }
+
+}
+
---
+++ Project Problem Solving/Assets/Scripts/Problem 10/BoxSpawner9.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner9 : MonoBehaviour
+{
+    //singletone structure
+    private static BoxSpawner9 instance = null;
+    public static BoxSpawner9 Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<BoxSpawner9>();
+                if (instance == null)
+                {
+                    GameObject go = new GameObject();
+                    go.name = " BoxSpawner";
+                    instance = go.AddComponent<BoxSpawner9>();
+                    DontDestroyOnLoad(go);
+                }
+            }
+            return instance;
+        }
+    }
+
+    [SerializeField] GameObject boxPrefab;
+    [SerializeField] int maxNumberOfBox;
+    [SerializeField] float areaWidth, areaHeight, objectSizeMax, respawnInterval;
+
+    private Dictionary<string, List<GameObject>> pool;
+    private List<GameObject> boxList;
+
+    float areaOffset;
+    int numberOfBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberOfBox = Random.Range(1, maxNumberOfBox + 1);
+        areaOffset = 0.5f;
+        spawnBox();
+    }
+
+    private void spawnBox()
+    {
+        pool = new Dictionary<string, List<GameObject>>();
+
+        GameObject box;
+
+        for (int i = 0; i < numberOfBox; i++)
+        {
+            box = GenerateFromPool(boxPrefab);
+            float randomScale = Random.Range(0, objectSizeMax) + 0.25f;
+            box.transform.localScale = new Vector2(randomScale, randomScale);
+        }
+    }
+    private GameObject GenerateFromPool(GameObject item)
+    {
+        Vector2 position = generateRandomPosition();
+
+        if (pool.ContainsKey(item.name))
+        {
+            if (pool[item.name].Count > 0)
+            {
+                GameObject newItemFromPool = pool[item.name][0];
+                pool[item.name].Remove(newItemFromPool);
+                newItemFromPool.SetActive(true);
+                newItemFromPool.transform.position = position;
+                return newItemFromPool;
+            }
+        }
+        else
+        {
+            pool.Add(item.name, new List<GameObject>());
+        }
+
+        GameObject newItem = Instantiate(item, position, Quaternion.identity);
+        newItem.name = item.name;
+        return newItem;
+    }
+
+    public void ReturnToPool(GameObject item)
+    {
+        if (!pool.ContainsKey(item.name))
+        {
+            Debug.LogError("INVALID POOL ITEM!!");
+        }
+        pool[item.name].Add(item);
+        item.SetActive(false);
+        StartCoroutine("spawn");
+
+    }
+
+    IEnumerator spawn()
+    {
+        yield return new WaitForSeconds(respawnInterval);
+        GenerateFromPool(boxPrefab);
+    }
+
+    private bool checkInsideSphere(Vector2 position)
+    {
+        GameObject avatar = GameObject.Find("Ball");
+        float distance = Vector2.Distance(position, avatar.transform.position);
+        if (distance < 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private Vector2 generateRandomPosition()
+    {
+        float x, y;
+        Vector2 position;
+        do
+        {
+            x = Random.RandomRange(-areaWidth + areaOffset, areaWidth - areaOffset);
+            y = Random.RandomRange(-areaHeight + areaOffset, areaHeight - areaOffset);
+            position = new Vector2(x, y);
+        } while (checkInsideSphere(position));
+        return position;
+
+    }
+}
